3d5532664fcd5bf4866165e4cf4a8369ccaac7d9
Luzimar Oliveira <lzm.spfc@gmail.com>
2021-04-20 11:26:38 -0400

    Fix component type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import { AppProps } from "next/app"
 import '../styles/globals.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
             <Component {...pageProps} />
